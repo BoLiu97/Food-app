5763345d524271e9c7dfc6919dd6ac6d9e513d8b
Bo Liu <daidaidashixiongnmsl@gmail.com>
2019-09-10 20:09:32 -0400

    clicking and jumping
    clicking the image and successfully jump to the detail page of the food.
    next step: click bottom button and jump to new activity and summary the food cal with cycle showing

--- app/src/main/java/com/example/burger_app/MainActivity.kt
+++ app/src/main/java/com/example/burger_app/MainActivity.kt
@@ -1,56 +1,102 @@
 package com.example.burger_app
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import androidx.core.content.ContextCompat.startActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.food_ticket.view.*
 
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.widget.Toast
+import java.net.URI
+
+
 class MainActivity : AppCompatActivity() {
-    var adapter:FoodAdapter?=null
+
+    //var adapter:FoodAdapter?=null
     var listOfFoods = ArrayList<food>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //load foods
-        listOfFoods.add(food("bacon_cheeseburger","https://www.bk.com/menu-item/bacon-cheeseburger",R.drawable.bacon_cheesburger))
-        listOfFoods.add(food("bacon_double_cheesburger","https://www.bk.com/menu-item/bacon-double-cheeseburger",R.drawable.double_cheeseburger))
-        listOfFoods.add(food("cheeseburger","https://www.bk.com/menu-item/cheeseburger",R.drawable.chessburger))
-        listOfFoods.add(food("chicken_jr","https://www.bk.com/menu-item/chicken-jr",R.drawable.chicken_jr))
-        listOfFoods.add(food("chicken_nuggets","https://www.bk.com/menu-item/chicken-nuggets",R.drawable.chicken_nuggets))
-        listOfFoods.add(food("coca_cola","https://www.bk.com/menu-item/coca-cola%C2%AE",R.drawable.coca_cola))
-        listOfFoods.add(food("diet_coke","https://www.bk.com/menu-item/diet-coke",R.drawable.diet_coke))
-        listOfFoods.add(food("double_cheeseburger","https://www.bk.com/menu-item/double-cheeseburger",R.drawable.double_cheeseburger))
-        listOfFoods.add(food("dr_pepper","https://www.bk.com/menu-item/dr-pepper",R.drawable.dr_pepper))
-        listOfFoods.add(food("french_fries","https://www.bk.com/menu-item/french-fries",R.drawable.french_fries))
-        listOfFoods.add(food("frozen_fanta_cherry_icee","https://www.bk.com/menu-item/frozen-fanta-cherry-icee",R.drawable.frozen_fanta_cherry_icee))
-        listOfFoods.add(food("onion_rings","https://www.bk.com/menu-item/onion-rings",R.drawable.onion_rings))
-        listOfFoods.add(food("spicy_chicken_jr","https://www.bk.com/menu-item/spicy-chicken-jr",R.drawable.spicy_chicken_jr))
-        listOfFoods.add(food("sprite","https://www.bk.com/menu-item/sprite",R.drawable.sprite))
-        listOfFoods.add(food("vanilla_soft_serve","https://www.bk.com/menu-item/vanilla-soft-serve",R.drawable.vanilla_soft_serve))
-        adapter= FoodAdapter(this,listOfFoods)
-        gvListFood.adapter = adapter
+        listOfFoods.add(food("bacon_cheeseburger", "https://www.bk.com/menu-item/bacon-cheeseburger",R.drawable.bacon_cheesburger,320,false))
+        listOfFoods.add(food("bacon_double_cheesburger","https://www.bk.com/menu-item/bacon-double-cheeseburger",R.drawable.double_cheeseburger,400,false))
+        listOfFoods.add(food("cheeseburger","https://www.bk.com/menu-item/cheeseburger",R.drawable.chessburger,280,false))
+        listOfFoods.add(food("chicken_jr","https://www.bk.com/menu-item/chicken-jr",R.drawable.chicken_jr,450,false))
+        listOfFoods.add(food("chicken_nuggets","https://www.bk.com/menu-item/chicken-nuggets",R.drawable.chicken_nuggets,170,false))
+        listOfFoods.add(food("coca_cola","https://www.bk.com/menu-item/coca-cola%C2%AE",R.drawable.coca_cola,140,false))
+        listOfFoods.add(food("diet_coke","https://www.bk.com/menu-item/diet-coke",R.drawable.diet_coke,0,false))
+        listOfFoods.add(food("double_cheeseburger","https://www.bk.com/menu-item/double-cheeseburger",R.drawable.double_cheeseburger,390,false))
+        listOfFoods.add(food("dr_pepper","https://www.bk.com/menu-item/dr-pepper",R.drawable.dr_pepper,140,false))
+        listOfFoods.add(food("french_fries","https://www.bk.com/menu-item/french-fries",R.drawable.french_fries,380,false))
+        listOfFoods.add(food("frozen_fanta_cherry_icee","https://www.bk.com/menu-item/frozen-fanta-cherry-icee",R.drawable.frozen_fanta_cherry_icee,110,false))
+        listOfFoods.add(food("onion_rings","https://www.bk.com/menu-item/onion-rings",R.drawable.onion_rings,150,false))
+        listOfFoods.add(food("spicy_chicken_jr","https://www.bk.com/menu-item/spicy-chicken-jr",R.drawable.spicy_chicken_jr,390,false))
+        listOfFoods.add(food("sprite","https://www.bk.com/menu-item/sprite",R.drawable.sprite,140,false))
+        listOfFoods.add(food("vanilla_soft_serve","https://www.bk.com/menu-item/vanilla-soft-serve",R.drawable.vanilla_soft_serve,140,false))
 
+
+        //adapter= FoodAdapter(this,listOfFoods)
+        //gvListFood.adapter = adapter
+        gvListFood.adapter = FoodAdapter(listOfFoods){
+            val openURL = Intent(Intent.ACTION_VIEW, Uri.parse(it))
+            startActivity(openURL)
+        }
+        btCC.setOnClickListener {
+            val checkedWebs = listOfFoods.filter{ it.checked }
+            //Toast.makeText(this, checkedWebs.joinToString(",") { it.name }, Toast.LENGTH_LONG)
+                //.show()
+        }
     }
-    class FoodAdapter: BaseAdapter {
-        var listOfFood = ArrayList<food>()
+
+    class FoodAdapter (val listOfFood:ArrayList<food>, val click: (String) -> Unit) :
+        BaseAdapter() {
+        //var listOfFood = ArrayList<food>()
+        /*
         var context:Context?=null
         constructor(context: Context,listOfFood:ArrayList<food>):super(){
             this.context=context
             this.listOfFood=listOfFood
+        */
 
-        }
         override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
             val food = this.listOfFood[p0]
-            var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            var foodView = inflator.inflate(R.layout.food_ticket,null)
+            var foodView =
+                LayoutInflater.from(p2?.context).inflate(R.layout.food_ticket, p2, false)
             foodView.ivFoodImg.setImageResource(food.image!!)
+            /*
+            foodView.ivFoodImg.setOnClickListener {
+                val url = food.des
+                val intent = Intent()
+                intent.action = Intent.ACTION_VIEW
+                intent.addCategory(Intent.CATEGORY_BROWSABLE)
+                intent.data = Uri.parse("")
+                startActivity(intent)
+                //val intent = Intent(Intent.ACTION_VIEW,Uri.parse(food.des),context,FoodDetails::class.java)
+                context!!.startActivity(intent)
+            }
+
+             */
+            //val click: (String) -> Unit
+            foodView.tvCalo.text=Integer.toString(food.Calo!!)
             foodView.tvName.text= food.name!!
+            foodView.ivFoodImg.setOnClickListener {
+                click(food.des)
+            }
+            foodView.cbChoose.setOnCheckedChangeListener { compoundButton, b ->
+                food.checked = b
+            }
             return foodView
 
         }
@@ -69,5 +115,6 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    }
 
-}
+
--- app/src/main/java/com/example/burger_app/food.kt
+++ app/src/main/java/com/example/burger_app/food.kt
@@ -1,14 +1,22 @@
 package com.example.burger_app
 
+import android.net.Uri
+import java.net.URI
+import java.net.URL
+
 class food{
     var name:String? = null
-    var des:String? = null
+    var des: String = ""
     var image:Int? = null
-    constructor(name:String,des:String,image:Int){
+    var Calo:Int? = null
+    var checked:Boolean= false
+
+    constructor(name:String,des:String,image:Int,Calo:Int,checked:Boolean){
         this.name=name
         this.des=des
         this.image=image
-
+        this.Calo = Calo
+        this.checked=checked
     }
 
 }
